7f865ed7435d08d56dbf3f03ff87aee55b3a6711
Graham Burris <hamwannabe17@gmail.com>
2020-09-07 11:35:06 -0400

    adds the lilliput ResizingList example code

--- TopTenCountryPopulations/Program.cs
+++ TopTenCountryPopulations/Program.cs
@@ -12,7 +12,12 @@ namespace TopTenCountryPopulations
 
             List<Country> countries = reader.ReadAllCountries();
 
-            foreach(Country country in countries)
+            Country lilliput = new Country("Lilliput", "LIL", "Somewhere", 2_000_000);
+            int lilliputIndex = countries.FindIndex(x => x.Population < 2_000_000);
+            countries.Insert(lilliputIndex, lilliput);
+            countries.RemoveAt(lilliputIndex);
+
+            foreach (Country country in countries)
             {
                 Console.WriteLine($"{country.Population} : {country.Name}");
             }
