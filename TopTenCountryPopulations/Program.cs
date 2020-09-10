bbfb30cb141c72c80c685c6f40bc85c1f18627cb
Graham Burris <hamwannabe17@gmail.com>
2020-09-10 11:55:57 -0400

    adds the brief interface example from the module

--- TopTenCountryPopulations/Program.cs
+++ TopTenCountryPopulations/Program.cs
@@ -10,7 +10,9 @@ namespace TopTenCountryPopulations
             string filePath = @"Pop by Largest Final.csv";
             CsvReader reader = new CsvReader(filePath);
 
-            List<Country> countries = reader.ReadAllCountries();
+            IList<Country> countries = reader.ReadAllCountries(); 
+            //using IList<T> you can change the ReadAllCountries method to return an array or 
+            //a list because they both implement IList<T>
 
             foreach(Country country in countries)
             {
